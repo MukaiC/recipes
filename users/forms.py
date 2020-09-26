681c2da795932cd0969d32d51e062192781b69b0
Chiharu Mukai <49642982+MukaiC@users.noreply.github.com>
2020-09-26 16:53:10 +0200

    Added user profile update form to profile page, limited image size

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from recipes.models import User
 from django.contrib.auth.forms import UserCreationForm
+from recipes.models import User
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=False)
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model =  User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -7,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        # if the image size is bigger than 300px, shrink it to output_size
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
